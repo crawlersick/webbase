41b1336c8959624d7918061988236e7277363e7c
John <john.b.y.yang@gmail.com>
2013-01-23 00:57:53 +0800

    just deleted invalid values and lines

--- src/networktool/DNSQ.java
+++ src/networktool/DNSQ.java
@@ -7,7 +7,6 @@ package networktool;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,37 +16,19 @@ import java.util.logging.Logger;
  * @author sick
  */
  public    class  DNSQ {
-    /*
-    public  DNSQ(String TargetDomainName,String Targetip){
-    this.TargetDomainName=TargetDomainName;
-    this.Targetip=Targetip;
-    }
-    */
-     public DNSQ(){
-     
-     }
-     
-     @SuppressWarnings("empty-statement")
     public String Getip( String DNSs,String TgDN) throws Exception{
-         int datasize;   
-         datasize=adddataend(0,b0,datahead);
-         
-            strarr=TgDN.split("\\.");
+         datasize=adddataend(0,b0,datahead);strarr=TgDN.split("\\.");
             for(String tempstr:strarr)
             {
                 x=tempstr.length();
                 datasize++;
                 b0[datasize-1]=(byte)x;
                datasize=adddataend(datasize,b0,tempstr.getBytes("ASCII"));
-               
             }
           datasize=adddataend(datasize,b0,dataend);
           byte[] tempbyby=ByteBuffer.allocate(4).putInt(datasize-2).array();
-          b0[0]=tempbyby[2];
-          b0[1]=tempbyby[3];
-          
-          
-            s=new Socket(DNSs,53);
+          b0[0]=tempbyby[2];b0[1]=tempbyby[3];
+          s=new Socket(DNSs,53);
   /*        
             String newfind= "00 1f 00 00 01 00 00 01 00 00 00 00 00 00 03 77 77 77 05 62 61 69 64 75 03 63 6f 6d 00 00 01 00 01";
             newfind=newfind.replaceAll(" ", "");
@@ -55,19 +36,12 @@ import java.util.logging.Logger;
             b1=HexCodec.hexToBytes(newfind.toCharArray());
       */      
             /*
-           for(int i=0;i<datasize;i++){
-                
-                    System.out.print(Integer.toHexString(b0[i])+" ");
-            }
+           for(int i=0;i<datasize;i++){                    System.out.print(Integer.toHexString(b0[i])+" ");            }
             System.out.println();
             */
-
-            osm=s.getOutputStream();
-            ism=s.getInputStream();
+            osm=s.getOutputStream();ism=s.getInputStream();
             osm.write(b0,0,datasize);
-            //osm.flush();
             //System.out.println("***************************************");
-            
             int n=0;
             while ((x=ism.read())!=-1)
             {
@@ -76,59 +50,45 @@ import java.util.logging.Logger;
             n++;
             }
             //System.out.println("\n***************************************");
-            
+            String returnvalue="";
             for(int i=0;i<n;i++)
             {
-                if(b3[i]==192
-                        &&
-                   b3[i+2]==0
-                        &&
-                   b3[i+3]==1
-                        &&
-                   b3[i+4]==0
-                        &&
-                   b3[i+5]==1
-                        ){
-                                System.out.println(b3[i+12]+"."+b3[i+13]+"."+b3[i+14]+"."+b3[i+15]);
+                if(b3[i]==192&&b3[i+2]==0&&b3[i+3]==1&&b3[i+4]==0&&b3[i+5]==1)
+                            {
+                                if(returnvalue.equals(""))
+                                    returnvalue=b3[i+12]+"."+b3[i+13]+"."+b3[i+14]+"."+b3[i+15];
+                                else
+                                    returnvalue=returnvalue+"|"+b3[i+12]+"."+b3[i+13]+"."+b3[i+14]+"."+b3[i+15];
+                               // System.out.println(b3[i+12]+"."+b3[i+13]+"."+b3[i+14]+"."+b3[i+15]);
                             }
             }
-                        
-    return null;
+    return returnvalue;
     }
     
     public static void main(String arg[]){
-         try {
-             
+         try { 
              DNSQ dq=new DNSQ();
-            //dq.Getip("202.96.128.166", "baidu.com");
-             // dq.Getip("8.8.8.8", "t66y.com");
-              dq.Getip("8.8.8.8", "www.youtube.com");
-              //dq.Getip("8.8.8.8", "plus.google.com");
-             //String aaa="asd";
-             //System.out.println(aaa.getBytes("ASCII").length);
-             
+              String a=
+                  //    dq.Getip("8.8.8.8", "www.youtube.com");
+                   dq.Getip("8.8.8.8", "t66y.com");
+              System.out.println(a);
          } catch (Exception ex) {
              Logger.getLogger(DNSQ.class.getName()).log(Level.SEVERE, null, ex);
          }
-    }
-    
+    }    
 
-    private int adddataend(int headnum,byte[]tb,byte[]tb_end)
-    {   
+    private int adddataend(int headnum,byte[]tb,byte[]tb_end) {   
             if(tb.length-headnum<tb_end.length)
             {
-                System.out.println("teststaesktjjjflajdasfkll");
+                System.out.println("data head is too long!");
                 return 0;
             }
             int i;
             for(i=headnum;i<headnum+tb_end.length;i++)
-            {
-  //              System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"+i);
-                tb[i]=tb_end[i-headnum];
-            }
+                  tb[i]=tb_end[i-headnum];
             return i;
-    }
-
+                                                                                                                           }
+    private int datasize;   
     private byte[] b0=new byte[1024];
     private byte[] datahead={0x00,0x1f,0x00,0x00,0x01,0x00,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00};
     private byte[]dataend={0x00,0x00,0x01,0x00,0x01};
